d796e08f5a9589c4d8a069507b421c986f126e2c
Graham Veal <grahamveal@gmail.com>
2019-10-14 08:54:58 +0100

    POC for routes and urls together in one module

--- test/unit/lib/urls.test.js
+++ test/unit/lib/urls.test.js
@@ -26,8 +26,18 @@ describe('urls', () => {
       companyId = faker.random.uuid()
     })
     it('should return the correct values', () => {
+      expect(urls.companies.collection.route).to.equal('/')
+      expect(urls.companies.collection()).to.equal('/companies')
+
+      expect(urls.companies.detail.route).to.equal('/:companyId')
       expect(urls.companies.detail(companyId)).to.equal(`/companies/${companyId}`)
+
+      expect(urls.companies.exports.route).to.equal('/:companyId/exports')
       expect(urls.companies.exports(companyId)).to.equal(`/companies/${companyId}/exports`)
+
+      const globalHqId = faker.random.uuid()
+      expect(urls.companies.hierarchies.ghq.add.route).to.equal('/:companyId/hierarchies/ghq/:globalHqId/add')
+      expect(urls.companies.hierarchies.ghq.add(companyId, globalHqId)).to.equal(`/companies/${companyId}/hierarchies/ghq/${globalHqId}/add`)
     })
   })
 })
