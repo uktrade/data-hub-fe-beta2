864bfec488089d86c4947915d90a839e00856404
Miriam Forner <mforner13@gmail.com>
2020-05-05 16:53:27 +0100

    Only show admin tab to users with permission

--- src/apps/investments/middleware/local-navigation.js
+++ src/apps/investments/middleware/local-navigation.js
@@ -12,10 +12,17 @@ const filterDocNavItemIfProjectIsNew = (investment) => {
 }
 
 const setLocalNavigation = (req, res, next) => {
-  const { investment } = res.locals
-  const navItems = investment
-    ? filterDocNavItemIfProjectIsNew(investment)
-    : LOCAL_NAV
+  const { investment, user } = res.locals
+  const userIsAdmin = user.hasPermission(
+    'investment.change_to_any_stage_investmentproject'
+  )
+  const checkShowAdminTab = (navItems) =>
+    userIsAdmin ? navItems : navItems.filter((item) => item.path !== 'admin')
+
+  const navItems = checkShowAdminTab(
+    investment ? filterDocNavItemIfProjectIsNew(investment) : LOCAL_NAV
+  )
+
   setLocalNav(navItems)(req, res, next)
 }
 
