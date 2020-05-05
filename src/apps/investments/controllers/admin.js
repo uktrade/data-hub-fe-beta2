547c9ff2ef72c757986cfa65f46ac708d43ee54f
Miriam Forner <mforner13@gmail.com>
2020-05-05 11:13:11 +0100

    Display stage options as radios

--- src/apps/investments/controllers/admin.js
+++ src/apps/investments/controllers/admin.js
@@ -1,12 +1,25 @@
-function renderAdminView(req, res) {
+const { getOptions } = require('../../../lib/options')
+
+async function renderAdminView(req, res) {
+  const { token } = req.session
   const { id, name, stage } = res.locals.investment
+  const orderStages = (stages) => {
+    const activeStage = stages[0]
+    stages[0] = stages[2]
+    stages[2] = activeStage
+    return stages
+  }
+  const stages = orderStages(
+    await getOptions(token, 'investment-project-stage')
+  )
 
   res.locals.title = `Admin - ${name} - Projects - Investments - DIT Data Hub`
   res.render('investments/views/admin/client-container.njk', {
     props: {
-      id,
-      name,
-      stage,
+      projectId: id,
+      projectName: name,
+      projectStage: stage,
+      stages,
     },
   })
 }
--- src/apps/investments/views/admin/client/InvestmentProjectAdmin.jsx
+++ src/apps/investments/views/admin/client/InvestmentProjectAdmin.jsx
@@ -1,24 +1,60 @@
 import React from 'react'
 
-import { Main } from 'govuk-react'
+import styled from 'styled-components'
+import { Main, InsetText, H4, Button, Link } from 'govuk-react'
+import { SPACING } from '@govuk-react/constants'
+import { FormStateful, FieldRadios, FormActions } from 'data-hub-components'
 import LocalHeader from '../../../../../client/components/LocalHeader.jsx'
 
 import { dashboard, investments } from '../../../../../lib/urls'
 
-const InvestmentProjectAdmin = ({ id, name }) => (
-  <>
-    <LocalHeader
-      heading={'Change the project stage'}
-      breadcrumbs={[
-        { link: dashboard(), text: 'Home' },
-        { link: investments.index(), text: 'Investments' },
-        { link: investments.projects.index(), text: 'Projects' },
-        { link: investments.projects.project(id), text: name },
-        { text: 'Admin' },
-      ]}
-    />
-    <Main></Main>
-  </>
-)
+const StyledP = styled('p')`
+  margin-bottom: ${SPACING.SCALE_2};
+`
+
+const InvestmentProjectAdmin = ({
+  projectId,
+  projectName,
+  projectStage,
+  stages,
+}) => {
+  const newStageOptions = stages.filter(
+    (stage) => stage.value != projectStage.id
+  )
+
+  return (
+    <>
+      <LocalHeader
+        heading={'Change the project stage'}
+        breadcrumbs={[
+          { link: dashboard(), text: 'Home' },
+          { link: investments.index(), text: 'Investments' },
+          { link: investments.projects.index(), text: 'Projects' },
+          { link: investments.projects.project(projectId), text: projectName },
+          { text: 'Admin' },
+        ]}
+      />
+      <Main>
+        <H4>Project Details</H4>
+        <InsetText>
+          <p>Project name: {projectName}</p>
+          <StyledP>Current stage: {projectStage.name}</StyledP>
+        </InsetText>
+        <FormStateful>
+          <H4>Change the stage to</H4>
+          <FieldRadios
+            name="Change the stage to"
+            required="Select a new stage"
+            options={newStageOptions}
+          />
+          <FormActions>
+            <Button>Save</Button>
+            <Link href={investments.projects.project(projectId)}>Cancel</Link>
+          </FormActions>
+        </FormStateful>
+      </Main>
+    </>
+  )
+}
 
 export default InvestmentProjectAdmin
