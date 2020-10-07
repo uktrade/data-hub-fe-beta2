a37604ef5e078fccee81fdb75e943147d2dc8bf0
Ian Leggett <contactleggett@gmail.com>
2020-10-07 10:15:38 +0100

    wip

--- src/apps/usage/index.js
+++ src/apps/usage/index.js
@@ -2,7 +2,5 @@ const router = require('express').Router()
 const renderUsage = require('./controllers')
 
 module.exports = {
-  router: router.get('/foo',
-  renderUsage
-  ),
+  router: router.get('/data-usage', renderUsage),
 }
