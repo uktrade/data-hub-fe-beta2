bf82eeb1fb82289d52af56f2ae5fbd474183db4a
Tom Kingston <tom@kingstonlabs.co.uk>
2021-01-12 14:33:27 +0000

    Add Gatsby 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import BaseTemplate from '../gatsby-templates/BaseTemplate'
+
+const NotFoundPage = () => <BaseTemplate>Sorry, page not found!</BaseTemplate>
+
+export default NotFoundPage
