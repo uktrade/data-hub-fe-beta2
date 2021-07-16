f02e59c0c87b938ded0de36525e16b5356f16691
Miriam Forner <mforner13@gmail.com>
2021-07-16 16:37:53 +0100

    Add api-proxy to intercept route

--- test/functional/cypress/specs/investments/opportunity-requirements-form-spec.js
+++ test/functional/cypress/specs/investments/opportunity-requirements-form-spec.js
@@ -70,7 +70,7 @@ describe('A capital opportunity without existing requirements', () => {
     it('should allow the user to fill in all fields', () => {
       cy.intercept(
         'PATCH',
-        `/v4/large-capital-opportunity/${incompleteOpportunity.id}`
+        `/api-proxy/v4/large-capital-opportunity/${incompleteOpportunity.id}`
       ).as('apiRequest')
 
       cy.get('#total_investment_sought').type('100')
