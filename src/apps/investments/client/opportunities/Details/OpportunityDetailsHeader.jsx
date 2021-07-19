6330501548a7c5666932ec4ecd083a53b482c4b2
Miriam Forner <mforner13@gmail.com>
2021-07-19 16:22:40 +0100

    Refactor opportunity header

--- src/apps/investments/client/opportunities/Details/OpportunityDetailsHeader.jsx
+++ src/apps/investments/client/opportunities/Details/OpportunityDetailsHeader.jsx
@@ -1,39 +1,41 @@
 import React from 'react'
-import { useSelector, shallowEqual } from 'react-redux';
 import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
 import urls from '../../../../../lib/urls'
 import { currencyGBP } from '../../../../../client/utils/number-utils'
 import LocalHeader from '../../../../../client/components/LocalHeader/LocalHeader'
 import LocalHeaderHeading from '../../../../../client/components/LocalHeader/LocalHeaderHeading'
 import LocalHeaderDetails from '../../../../../client/components/LocalHeaderDetails'
+import { TASK_GET_OPPORTUNITY_DETAILS, ID, state2props } from '../Details/state'
+import Task from '../../../../../client/components/Task'
 
-const OpportunityDetailsHeader = ({ opportunityId }) => {
-  /** Used redux-hooks to connect the state were reducers action 
-      has been initiated from opportunity-details.  */ 
-  const details = useSelector(state => (state.opportunityDetails.details), shallowEqual);
-  const {
-    createdOn,
-    name,
-    ukRegions,
-    assetClasses,
-    opportunityValue,
-  } = details.detailsFields
-
-  const statusLabel = details.incompleteDetailsFields > 0 ? 'Unassigned' : 'Seeking investment'
-  const oppsValue = opportunityValue.value == null ? 'Not yet valued' : `${currencyGBP(opportunityValue.value)}`;
-  const ukLocation = ukRegions.length == 0 ? 'Not yet defined' :
-                        ukRegions.length > 1 ? 'Multiple' : `${ukRegions.map(v => v.label)}`;
-  const assetClass = assetClasses.length == 0 ? 'Not yet defined' : 
-                        assetClasses.length > 1 ? 'Multiple' : `${assetClasses.map(c => c.label)}`;
+const OpportunityDetailsHeader = (state) => {
+  const { createdOn, name, ukRegions, assetClasses, opportunityValue } =
+    state.details.detailsFields
+  const oppValue =
+    opportunityValue.value == null
+      ? 'Not yet valued'
+      : `${currencyGBP(opportunityValue.value)}`
+  const ukLocation =
+    ukRegions.length == 0
+      ? 'Not yet defined'
+      : ukRegions.length > 1
+      ? 'Multiple'
+      : `${ukRegions.map((v) => v.label)}`
+  const assetClass =
+    assetClasses.length == 0
+      ? 'Not yet defined'
+      : assetClasses.length > 1
+      ? 'Multiple'
+      : `${assetClasses.map((c) => c.label)}`
 
   const itemCollection = {
-    'Status': [{'label': `${statusLabel}`, 'value': ''}],
-    'Value': `${oppsValue}`,
-    'UK location': `${ukLocation}`,
-    'Asset value': `${assetClass}`,
-    'Created on': `${createdOn}`,
+    Status: state.details.status?.label,
+    Value: oppValue,
+    'UK location': ukLocation,
+    'Asset class': assetClass,
+    'Created on': createdOn,
   }
-
   const breadcrumbs = [
     { link: urls.dashboard(), text: 'Home' },
     { link: urls.investments.index(), text: 'Investments' },
@@ -42,17 +44,25 @@ const OpportunityDetailsHeader = ({ opportunityId }) => {
   ]
 
   return (
-    <>
-      { name && <LocalHeader breadcrumbs={breadcrumbs} flashMessages={null} >
-        <LocalHeaderHeading data-auto-id="heading">{name}</LocalHeaderHeading>
-        <LocalHeaderDetails items={itemCollection}/>
-      </LocalHeader> }
-    </>
+    <Task.Status name={TASK_GET_OPPORTUNITY_DETAILS} id={ID}>
+      {() => (
+        <>
+          {name && (
+            <LocalHeader breadcrumbs={breadcrumbs} flashMessages={null}>
+              <LocalHeaderHeading data-auto-id="heading">
+                {name}
+              </LocalHeaderHeading>
+              <LocalHeaderDetails items={itemCollection} />
+            </LocalHeader>
+          )}
+        </>
+      )}
+    </Task.Status>
   )
 }
 
 OpportunityDetailsHeader.propTypes = {
-  opportunityId: PropTypes.string.isRequired
+  opportunityId: PropTypes.string.isRequired,
 }
 
-export default OpportunityDetailsHeader
+export default connect(state2props)(OpportunityDetailsHeader)
